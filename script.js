d95e29870412848131d3b0bf42355b7c675cc321
Pedro Lucas <Pedro Lucas pedrolucaslopesmonteiro43@gmail.com>
2026-03-17 11:29:33 -0300

    Atualizando assets

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
     buttonEl.addEventListener("click", () => {
         window.location.href = "login.html"
     });
+
+    const buttonS1 = document.querySelector("#services1")
+    buttonS1.addEventListener("click", () => {
+        window.location.href = "login.html"
+    }); 
