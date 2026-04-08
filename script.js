025c902cd2feb33dccdf1eb32e25e9524120573d
Pedro Lucas <Pedro Lucas pedrolucaslopesmonteiro43@gmail.com>
2026-04-08 11:06:55 -0300

    assests' file

--- script.js
+++
@@ -1,9 +0,0 @@
-    const buttonEl = document.querySelector("#autentication")
-    buttonEl.addEventListener("click", () => {
-        window.location.href = "login.html"
-    });
-
-    const buttonS1 = document.querySelector("#services1")
-    buttonS1.addEventListener("click", () => {
-        window.location.href = "login.html"
-    }); 
